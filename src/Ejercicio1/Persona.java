205f223ae51e0d110041e7217a74ca5c8374f87e
Cherry708 <aluser8241@ieselcaminas.org>
2021-04-19 20:42:19 +0200

    Initial commit

---
+++ src/Ejercicio1/Persona.java
@@ -0,0 +1,13 @@
+package Ejercicio1;
+
+public class Persona {
+
+    //Atributos
+    String nombre;
+    int edad;
+
+    //Métodos
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+}
